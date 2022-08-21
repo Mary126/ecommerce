b5238ed987fe3aff872c636ca75f0115fde22b44
Mary126 <marussia26@hotmail.com>
2022-08-21 12:17:04 +0300

    add cart screen logic

--- lib/pages/cart_screen.dart
+++ lib/pages/cart_screen.dart
@@ -24,6 +24,7 @@ class _CartScreen extends State<CartScreen> {
           backgroundColor: ColorConstant.grayColor,
           body: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
                 margin: const EdgeInsets.all(20),
@@ -76,52 +77,138 @@ class _CartScreen extends State<CartScreen> {
                   borderRadius: const BorderRadius.all(Radius.circular(15)),
                   color: ColorConstant.blueColor,
                 ),
+                padding: const EdgeInsets.all(20),
                 child: Column(
                   children: [
-                    FutureBuilder<Map<String, dynamic>>(
-                      future: cart,
-                      builder: (context, snapshot) {
-                        return snapshot.hasData ? Column(
-                          children: [
-                            for (int i = 0; i < snapshot.data!['basket'].length; i++) Container(
-                              margin: const EdgeInsets.all(20),
-                              child: Row(
+                    SizedBox(
+                      height: 350,
+                      child: SingleChildScrollView(
+                        child: FutureBuilder<Map<String, dynamic>>(
+                            future: cart,
+                            builder: (context, snapshot) {
+                              return snapshot.hasData ? Column(
                                 children: [
-                                  SizedBox(
-                                    height: 60,
-                                    width: 60,
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(10.0),
-                                      child: Image.network(
+                                  for (int i = 0; i < snapshot.data!['basket'].length; i++)
+                                    cartItem(
                                         snapshot.data!['basket'][i]['images'].toString(),
-                                        fit: BoxFit.cover,
-                                      ),
+                                        snapshot.data!['basket'][i]['title'],
+                                        snapshot.data!['basket'][i]['price'].toString()
                                     )
+                                ],
+                              ) : Container();
+                            }
+                        ),
+                      )
+                    ),
+                    const Divider(color: Colors.white,),
+                    FutureBuilder<Map<String, dynamic>>(
+                        future: cart,
+                        builder: (context, snapshot) {
+                          return snapshot.hasData ? Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Padding(
+                                    padding: EdgeInsets.only(top:10, bottom: 10),
+                                    child: Text("Total", style: TextStyle(color: Colors.white, fontSize: 15),),
                                   ),
-                                  Container(
-                                    margin: const EdgeInsets.only(left: 10, right: 10),
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Text(snapshot.data!['basket'][i]['title'], style: const TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),),
-                                        Text("\$${snapshot.data!['basket'][i]['price'].toString()}", style: TextStyle(color: ColorConstant.orangeColor, fontSize: 20, fontWeight: FontWeight.w500),)
-                                      ],
-                                    ),
-                                  )
-
+                                  Text("Delivery", style: TextStyle(color: Colors.white, fontSize: 15),),
                                 ],
                               ),
-                            )
-                          ],
-                        ) : Container();
-                      }
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const  EdgeInsets.only(top:10, bottom: 10),
+                                    child: Text("\$${snapshot.data!['total'].toString()} us", style: const TextStyle(color: Colors.white, fontSize: 15),),
+                                  ),
+                                  Text(snapshot.data!['delivery'].toString(), style: const TextStyle(color: Colors.white, fontSize: 15),)
+                                ],
+                              )
+                            ],
+                          ) : Container();
+                        }
                     ),
+                    const Divider(color: Colors.white),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+                      child: ElevatedButton(
+                        onPressed: () {
+
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(ColorConstant.orangeColor),
+                          minimumSize: MaterialStateProperty.all(const Size(double.infinity, 40)),
+                        ),
+                        child: const Text(
+                          "Checkout",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    )
                   ],
                 ),
               )
             ],
           ),
         )
+    );
+  }
+  Container cartItem(String imageUrl, String title, String price) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                  height: 60,
+                  width: 60,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: Image.network(
+                      imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  )
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 10, right: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(title, style: const TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w400),),
+                    Text("\$$price", style: TextStyle(color: ColorConstant.orangeColor, fontSize: 20, fontWeight: FontWeight.w400),)
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: const Color.fromRGBO(40, 40, 67, 1)
+                ),
+                margin: const EdgeInsets.only(right: 10),
+                padding: const EdgeInsets.all(5),
+                child: Column(
+                  children: const[
+                    Icon(Icons.remove, color: Colors.white, size: 15),
+                    Text("2", style: TextStyle(color: Colors.white),),
+                    Icon(Icons.add, color: Colors.white, size: 15,),
+                  ],
+                ),
+              ),
+              Image.asset("assets/images/bin.png")
+            ],
+          )
+        ],
+      ),
     );
   }
   Future<Map<String, dynamic>> fetchCart() async {
